39222695f3e94b97fcdcf5afb13d5cfb6fd65cea
Daniel Carreon <danielcarreong00@gmail.com>
2023-10-25 07:50:03 -0600

    some changes

--- src/scripts/used_functions.py
+++ src/scripts/used_functions.py
@@ -1,9 +1,5 @@
 import pandas as pd
 import numpy as np
-
-pd.set_option("display.max_rows", 50)
-pd.set_option("display.max_columns", 50)
-pd.options.display.float_format = "{:,.2f}".format
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -108,5 +104,184 @@ def plot_sales_by_market(df):
     plt.ylabel("Total Gross Sales")
     plt.legend(loc="upper left")
     plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_sales_by_customer(df):
+    """
+    Plots the total gross sales over time for each customer in Chile.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing at least the 'date', 'customer', and 'total_gross_sales' columns.
+
+    Returns:
+        None
+    """
+    # Grouping by date and customer
+    sales_by_date_customer = (
+        df.groupby(["date", "customer"])["total_gross_sales"].sum().reset_index()
+    )
+    plt.figure(figsize=(15, 7))
+
+    # Loop over each customer to plot its time series
+    for customer in sales_by_date_customer["customer"].unique():
+        subset = sales_by_date_customer[sales_by_date_customer["customer"] == customer]
+        plt.plot(subset["date"], subset["total_gross_sales"], label=customer)
+
+    plt.title("Sales Over Time by Seller")
+    plt.xlabel("Date")
+    plt.ylabel("Total Gross Sales")
+    plt.legend(loc="upper left")
+    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_sales_by_category_and_customer(df):
+    """
+    Plots the total gross sales by category and customer.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing at least the 'category', 'customer', and 'total_gross_sales' columns.
+
+    Returns:
+        None
+    """
+    # Grouping by category and customer, and calculating the total gross sales
+    category_sales_by_customer = (
+        df.groupby(["category", "customer"])
+        .agg(total_gross_sales=("total_gross_sales", "sum"))
+        .reset_index()
+        .sort_values("total_gross_sales", ascending=False)
+    )
+
+    # Plotting the data
+    plt.figure(figsize=(14, 7))
+    sns.barplot(
+        data=category_sales_by_customer,
+        x="category",
+        y="total_gross_sales",
+        hue="customer",
+        palette="viridis",
+    )
+    plt.title("Total Gross Sales by Category and Customer in Chile")
+    plt.ylabel("Total Gross Sales")
+    plt.xlabel("Category")
+    plt.xticks(rotation=45)
+    plt.legend(title="Customer")
+    plt.tight_layout()
+    plt.show()
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+def plot_sales_category_matrix(df):
+    """
+    Plots a matrix of total gross sales by category and customer over time.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing at least the 'date', 'category', 'customer', and 'total_gross_sales' columns.
+
+    Returns:
+        None
+    """
+    grouped_data = (
+        df.groupby(["date", "category", "customer"])["total_gross_sales"]
+        .sum()
+        .reset_index()
+    )
+    grouped_data["date"] = pd.to_datetime(grouped_data["date"])
+    grouped_data = grouped_data.sort_values(by="date")
+    categories = grouped_data["category"].unique()
+    n_cols = 3
+    n_rows = int(np.ceil(len(categories) / n_cols))
+
+    # Create the plot matrix
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(20, 5 * n_rows), sharex=True)
+
+    # Iterate over each category and place it in the matrix
+    for idx, category in enumerate(categories):
+        row = idx // n_cols
+        col = idx % n_cols
+        ax = axes[row, col]
+        subset = grouped_data[grouped_data["category"] == category]
+        lines = []  # Store lines for the legend
+        labels = []  # Store labels for the legend
+        for customer in subset["customer"].unique():
+            customer_data = subset[subset["customer"] == customer]
+            (line,) = ax.plot(
+                customer_data["date"],
+                customer_data["total_gross_sales"],
+                label=customer,
+                marker="o",
+            )
+            lines.append(line)
+            labels.append(customer)
+        ax.set_title(f"Sales: {category}")
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Total Sales")
+        ax.grid(True)
+        ax.tick_params(axis="x", rotation=45)
+
+    # Adjust layout and add a global legend
+    fig.tight_layout()
+    fig.legend(
+        lines, labels, loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=len(labels)
+    )
+    plt.show()
+
+
+def plot_sales_by_category(df):
+    """
+    Plots total gross sales over time grouped by category and customer.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing at least the 'date', 'category', 'customer', and 'total_gross_sales' columns.
+
+    Returns:
+        None
+    """
+    grouped_data = (
+        df.groupby(["date", "category", "customer"])["total_gross_sales"]
+        .sum()
+        .reset_index()
+    )
+    grouped_data["date"] = pd.to_datetime(grouped_data["date"])
+    grouped_data = grouped_data.sort_values(by="date")
+    categories = grouped_data["category"].unique()
+    line_styles = ["-", "--", "-.", ":"]
+
+    # Set Seaborn style and color palette
+    sns.set(style="whitegrid")
+    palette = sns.color_palette("inferno", n_colors=len(categories))
+
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(15, 8))
+
+    for idx, category in enumerate(categories):
+        subset = grouped_data[grouped_data["category"] == category]
+        for j, customer in enumerate(subset["customer"].unique()):
+            customer_data = subset[subset["customer"] == customer]
+            sns.lineplot(
+                data=customer_data,
+                x="date",
+                y="total_gross_sales",
+                label=f"{category} - {customer}",
+                color=palette[idx],
+                linestyle=line_styles[j % len(line_styles)],
+                ax=ax,
+            )
+
+    ax.set_title("Atliq Sales in Chile Over Time by Category and Customer")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Total Sales")
+    ax.legend(loc="upper left", bbox_to_anchor=(1, 1))
+    ax.grid(True)
+    ax.tick_params(axis="x", rotation=45)
+
     plt.tight_layout()
     plt.show()
